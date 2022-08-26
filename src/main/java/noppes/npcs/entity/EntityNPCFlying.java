3255a9c46969e59aee54ccdeb141ebd8ce740a4b
KAMKEEL <KamronVaswani@gmail.com>
2022-08-26 16:07:49 -0400

    Flying Movement Remake

--- src/main/java/noppes/npcs/entity/EntityNPCFlying.java
+++ src/main/java/noppes/npcs/entity/EntityNPCFlying.java
@@ -1,6 +1,8 @@
 package noppes.npcs.entity;
 
+import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
 public abstract class EntityNPCFlying extends EntityNPCInterface {
@@ -31,12 +33,67 @@ public abstract class EntityNPCFlying extends EntityNPCInterface {
         return false;
     }
 
+    @Override
     public void moveEntityWithHeading(float p_70612_1_, float p_70612_2_)
     {
         if(!this.canFly() || this.hurtTime != 0) {
             super.moveEntityWithHeading(p_70612_1_, p_70612_2_);
             return;
         }
+
+        if(isInWater()) {
+            moveFlying(p_70612_1_, p_70612_2_, 0.02F);
+            moveEntity(motionX, motionY, motionZ);
+            motionX *= 0.800000011920929D;
+            motionY *= 0.800000011920929D;
+            motionZ *= 0.800000011920929D;
+        } else if(handleLavaMovement()) {
+            moveFlying(p_70612_1_, p_70612_2_, 0.02F);
+            moveEntity(motionX, motionY, motionZ);
+            motionX *= 0.5D;
+            motionY *= 0.5D;
+            motionZ *= 0.5D;
+        } else {
+            float f2 = 0.91F;
+
+            if (onGround)  {
+                f2 = 0.54600006F;
+                Block block = worldObj.getBlock(MathHelper.floor_double(posX), MathHelper.floor_double(boundingBox.minY) - 1, MathHelper.floor_double(posZ));
+                f2 = block.slipperiness * 0.91F;
+            }
+
+            float f3 = 0.16277136F / (f2 * f2 * f2);
+            moveFlying(p_70612_1_, p_70612_2_, onGround ? 0.1F * f3 : 0.02F);
+            f2 = 0.91F;
+
+            if (onGround) {
+                f2 = 0.54600006F;
+                Block block = worldObj.getBlock(MathHelper.floor_double(posX), MathHelper.floor_double(boundingBox.minY) - 1, MathHelper.floor_double(posZ));
+                f2 = block.slipperiness * 0.91F;
+            }
+
+            moveEntity(motionX, motionY, motionZ);
+            motionX *= f2;
+            motionY *= f2;
+            motionZ *= f2;
+        }
+
+        prevLimbSwingAmount = limbSwingAmount;
+        double d0 = posX - prevPosX;
+        double d1 = posZ - prevPosZ;
+        float f4 = MathHelper.sqrt_double(d0 * d0 + d1 * d1) * 4.0F;
+
+        if(f4 > 1.0F)
+            f4 = 1.0F;
+
+        limbSwingAmount += (f4 - limbSwingAmount) * 0.4F;
+        limbSwing += limbSwingAmount;
+
+        this.velocityChanged = true;
+    }
+
+    public void moveEntityWithHeadingOld(float p_70612_1_, float p_70612_2_)
+    {
 
         double heightOffGround = 0;
         if(this.ai.hasFlyLimit) {
